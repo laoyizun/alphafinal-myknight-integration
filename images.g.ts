09502e23b877bd994ad1ed2eced9b860ede6cc1b
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-07-29 10:35:20 +0800

    Create images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,182 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+....ffffffffff.....
+...fffffffffffff...
+..fffffffffffffff..
+.fddddddddddddddff.
+ffdfffffdddfffffdff
+fdfdddddfdfdddddfdf
+fdfdddddfdfdddddfdf
+fffdd2ddfffdd2ddfdf
+fdfdddddfdfdddddfdf
+fdfdddddfdfdddddfdf
+dddfffffdddfffffddf
+ddddddddddddddddddd
+ddddddddddddddddddd
+dddffdddddddddfdddd
+.dddffdddddddffddd.
+.ddddffdddddffdddd.
+..ddddfffffffdddd..
+...ddddddddddddd...
+.....ddddddddd.....
+...................
+`, img`
+...................
+....ffffffffff.....
+...fffffffffffff...
+..fffffffffffffff..
+.fddddddddddddddff.
+ffdfffffdddfffffdff
+fdfdddddfdfdddddfdf
+fdfdddddfdfdddddfdf
+fffdd2ddfffdd2ddfdf
+fdfdddddfdfdddddfdf
+fdfdddddfdfdddddfdf
+dddfffffdddfffffddf
+ddddddddddddddddddd
+ddddddddddddddddddd
+dddffdddddddddfdddd
+.dddffdddddddffddd.
+.ddddffdddddffdddd.
+..ddddfffffffdddd..
+...ddddddddddddd...
+.....ddddddddd.....
+`];
+            case "myAnim0":
+            case "anim2":return [img`
+.....ffffffffff....
+...ffffffffffffff..
+..fffffffffffffff..
+.fffffffffffffffff.
+.fffffffffffffffff.
+fffffffffffffffffff
+fffffffffffffffffff
+fffffffffffffffffff
+fffffffffffffffffff
+dffffffffffffffffff
+dddfffffffffffffffd
+ddddfffffffffffffdd
+ddddffffffffffffddd
+dddddffffffffffdddd
+.dddddddfffffffddd.
+.ddddddddddddddddd.
+..ddddddddddddddd..
+...ddddddddddddd...
+.....ddddddddd.....
+...................
+`, img`
+...................
+.....ffffffffff....
+...ffffffffffffff..
+..fffffffffffffff..
+.fffffffffffffffff.
+.fffffffffffffffff.
+fffffffffffffffffff
+fffffffffffffffffff
+fffffffffffffffffff
+fffffffffffffffffff
+dffffffffffffffffff
+dddfffffffffffffffd
+ddddfffffffffffffdd
+ddddffffffffffffddd
+dddddffffffffffdddd
+.dddddddfffffffddd.
+.ddddddddddddddddd.
+..ddddddddddddddd..
+...ddddddddddddd...
+.....ddddddddd.....
+`];
+            case "myAnim1":
+            case "anim3":return [img`
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . f f f f f f f f f f f . . . 
+. . f f f f f d d d f f f f . . 
+. . d d d d d d d d d d d d . . 
+. . d f f f f f f f f f f d d . 
+. . d f f f f f f f f f f f d . 
+. . d f f f 2 f f f 2 f f d d . 
+. . . d f f f f f f f f f d . . 
+. . . d d d d d d d d d d d . . 
+. . . d d d d f d f d d d d . . 
+. . . . . d d f f f d d d . . . 
+. . . . . d d d d d d d . . . . 
+. . . . . . . . d d d . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . f f f f f f f f f f f . . . 
+. . f f f f f d d d f f f f . . 
+. . d d d d d d d d d d d d . . 
+. . d f f f f f f f f f f d d . 
+. . d f f f f f f f f f f f d . 
+. . d f f f 2 f f f 2 f f d d . 
+. . . d f f f f f f f f f d . . 
+. . . d d d d d d d d d d d . . 
+. . . d d d d f d f d d d d . . 
+. . . . . d d f f f d d d . . . 
+. . . . . d d d d d d d . . . . 
+. . . . . . . . d d d . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+            case "myAnim2":
+            case "anim4":return [img`
+. . . f f f f f f f f . . . . . 
+. . f f f f f f f f f f . . . . 
+. f f f f f f f f f f f f . . . 
+. f f f f f f f f f f f f . . . 
+f f f f f f f f f f f f f . . . 
+f f f f f f f f f f f f f . . . 
+d d d f f f f f f f d f d . . . 
+d d d d d d d d d d d d d . . . 
+d d d d d d d d d d d d d . . . 
+. d d d d d d d d d d d d . . . 
+. d d d d d d d d d d d . . . . 
+. d d d d d d d d d d d . . . . 
+. . d d d d d d d d d d . . . . 
+. . d d d d d d d d d . . . . . 
+. . d d d d d d d d d . . . . . 
+. . . d d d d d d . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f . . . . . 
+. . f f f f f f f f f f . . . . 
+. f f f f f f f f f f f f . . . 
+. f f f f f f f f f f f f . . . 
+f f f f f f f f f f f f f . . . 
+f f f f f f f f f f f f f . . . 
+d d d f f f f f f f d f d . . . 
+d d d d d d d d d d d d d . . . 
+d d d d d d d d d d d d d . . . 
+. d d d d d d d d d d d d . . . 
+. d d d d d d d d d d d . . . . 
+. d d d d d d d d d d d . . . . 
+. . d d d d d d d d d d . . . . 
+. . d d d d d d d d d . . . . . 
+. . d d d d d d d d d . . . . . 
+. . . d d d d d d . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
