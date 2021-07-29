e1ddd398c4cbf13ad0666cb112bdac1b342d2c66
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-07-29 21:47:27 +0800

    Update images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -1,4 +1,4 @@
-// 自动生成的代码。请勿编辑。
+// Auto-generated code. Do not edit.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
@@ -10,173 +10,278 @@ namespace myImages {
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "myAnim":
-            case "anim1":return [img`
-....ffffffffff.....
-...fffffffffffff...
-..fffffffffffffff..
-.fddddddddddddddff.
-ffdfffffdddfffffdff
-fdfdddddfdfdddddfdf
-fdfdddddfdfdddddfdf
-fffdd2ddfffdd2ddfdf
-fdfdddddfdfdddddfdf
-fdfdddddfdfdddddfdf
-dddfffffdddfffffddf
-ddddddddddddddddddd
-ddddddddddddddddddd
-dddffdddddddddfdddd
-.dddffdddddddffddd.
-.ddddffdddddffdddd.
-..ddddfffffffdddd..
-...ddddddddddddd...
-.....ddddddddd.....
+            case "myAnim3":
+            case "anim5":return [img`
+....bbbbbbbbbb.....
+...bbbbbbbbbbbbb...
+..bbbbbbbbbbbbbbbb.
+.bbbbbbbbbbbbbbbbbb
+bbbbbbbb4f44bbbbbbb
+bbbbbbb44f4444bbbbb
+bbbbbbf44f444f4bbbb
+bbbbb4f44f44ff4bbbb
+bbbb44f44f44f444bbb
+bb4444f44f44f4444bb
+b44444f44f4ff44444b
+bffffffffffffffff44
+444444f44f4f4444fff
+444444f44f4ff444444
+.4444ff44f44f44444.
+.4444f444f44ff4444.
+..44ff444f444ff44..
+...ff4444f4444ff...
+.....4444f4444.....
 ...................
 `, img`
 ...................
-....ffffffffff.....
-...fffffffffffff...
-..fffffffffffffff..
-.fddddddddddddddff.
-ffdfffffdddfffffdff
-fdfdddddfdfdddddfdf
-fdfdddddfdfdddddfdf
-fffdd2ddfffdd2ddfdf
-fdfdddddfdfdddddfdf
-fdfdddddfdfdddddfdf
-dddfffffdddfffffddf
-ddddddddddddddddddd
-ddddddddddddddddddd
-dddffdddddddddfdddd
-.dddffdddddddffddd.
-.ddddffdddddffdddd.
-..ddddfffffffdddd..
-...ddddddddddddd...
-.....ddddddddd.....
+....bbbbbbbbbb.....
+...bbbbbbbbbbbbb...
+..bbbbbbbbbbbbbbbb.
+.bbbbbbbbbbbbbbbbbb
+bbbbbbbb4f44bbbbbbb
+bbbbbbb44f4444bbbbb
+bbbbbbf44f444f4bbbb
+bbbbb4f44f44ff4bbbb
+bbbb44f44f44f444bbb
+bb4444f44f44f4444bb
+b44444f44f4ff44444b
+bffffffffffffffff44
+444444f44f4f4444fff
+444444f44f4ff444444
+.4444ff44f44f44444.
+.4444f444f44ff4444.
+..44ff444f444ff44..
+...ff4444f4444ff...
+.....4444f4444.....
 `];
-            case "myAnim0":
-            case "anim2":return [img`
-.....ffffffffff....
-...ffffffffffffff..
-..fffffffffffffff..
-.fffffffffffffffff.
-.fffffffffffffffff.
-fffffffffffffffffff
-fffffffffffffffffff
-fffffffffffffffffff
-fffffffffffffffffff
-dffffffffffffffffff
-dddfffffffffffffffd
-ddddfffffffffffffdd
-ddddffffffffffffddd
-dddddffffffffffdddd
-.dddddddfffffffddd.
-.ddddddddddddddddd.
-..ddddddddddddddd..
-...ddddddddddddd...
-.....ddddddddd.....
+            case "myAnim4":
+            case "anim6":return [img`
+.....bbbbbbbbb.....
+...bbbbbbbbbbbbb...
+..bbbbbbbbbbbbbbb..
+.bbbbbbbbbbbbbbbbb.
+.bbbbbbbbbbbbbbbbb.
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbb4
+bfffffffffbbbbbb44b
+444444f44f444ff4444
+.44444f44f444ff444.
+.44444f44ff444f444.
+..4fff444f4444ff4..
+...444444f44444ff..
+.....444444444.....
 ...................
 `, img`
 ...................
-.....ffffffffff....
-...ffffffffffffff..
-..fffffffffffffff..
-.fffffffffffffffff.
-.fffffffffffffffff.
-fffffffffffffffffff
-fffffffffffffffffff
-fffffffffffffffffff
-fffffffffffffffffff
-dffffffffffffffffff
-dddfffffffffffffffd
-ddddfffffffffffffdd
-ddddffffffffffffddd
-dddddffffffffffdddd
-.dddddddfffffffddd.
-.ddddddddddddddddd.
-..ddddddddddddddd..
-...ddddddddddddd...
-.....ddddddddd.....
+.....bbbbbbbbb.....
+...bbbbbbbbbbbbb...
+..bbbbbbbbbbbbbbb..
+.bbbbbbbbbbbbbbbbb.
+.bbbbbbbbbbbbbbbbb.
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbb4
+bfffffffffbbbbbb44b
+444444f44f444ff4444
+.44444f44f444ff444.
+.44444f44ff444f444.
+..4fff444f4444ff4..
+...444444f44444ff..
+.....444444444.....
 `];
-            case "myAnim1":
-            case "anim3":return [img`
-. . . . . f f f f f f . . . . . 
-. . . . f f f f f f f f . . . . 
-. . . f f f f f f f f f f . . . 
-. . f f f f f f f f f f f . . . 
-. . f f f f f d d d f f f f . . 
-. . d d d d d d d d d d d d . . 
-. . d f f f f f f f f f f d d . 
-. . d f f f f f f f f f f f d . 
-. . d f f f 2 f f f 2 f f d d . 
-. . . d f f f f f f f f f d . . 
-. . . d d d d d d d d d d d . . 
-. . . d d d d f d f d d d d . . 
-. . . . . d d f f f d d d . . . 
-. . . . . d d d d d d d . . . . 
-. . . . . . . . d d d . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+            case "myAnim6":
+            case "anim8":return [img`
+. . . . . . . . . . . . . . . 
+. . f f f f f f f f f f f . . 
+. f f f d d d d d d d f f f . 
+f f f f f f f d d f f f f f f 
+f f d d d d d f f d d d d f f 
+f f d d d d d f f d d d d d f 
+f f d d 2 d d f f d d 2 d d f 
+d f d d d d d f f d d d d d f 
+d f d d d d d f f d d d d d f 
+d d f f f f f d d f f f f f d 
+d d d d d d d d d d d d d d d 
+d d d d f d d d d d d f d d d 
+. d d d f d d d d d f f d d . 
+. d d d f f f d d f f d d d . 
+. . d d d d f f f f d d d . . 
+. . . . d d d d d d d . . . . 
 `, img`
-. . . . . . . . . . . . . . . . 
-. . . . . f f f f f f . . . . . 
-. . . . f f f f f f f f . . . . 
-. . . f f f f f f f f f f . . . 
-. . f f f f f f f f f f f . . . 
-. . f f f f f d d d f f f f . . 
-. . d d d d d d d d d d d d . . 
-. . d f f f f f f f f f f d d . 
-. . d f f f f f f f f f f f d . 
-. . d f f f 2 f f f 2 f f d d . 
-. . . d f f f f f f f f f d . . 
-. . . d d d d d d d d d d d . . 
-. . . d d d d f d f d d d d . . 
-. . . . . d d f f f d d d . . . 
-. . . . . d d d d d d d . . . . 
-. . . . . . . . d d d . . . . . 
-. . . . . . . . . . . . . . . . 
+. . f f f f f f f f f f f . . 
+. f f f d d d d d d d f f f . 
+f f f f f f f d d f f f f f f 
+f f d d d d d f f d d d d f f 
+f f d d d d d f f d d d d d f 
+f f d d 2 d d f f d d 2 d d f 
+d f d d d d d f f d d d d d f 
+d f d d d d d f f d d d d d f 
+d d f f f f f d d f f f f f d 
+d d d d d d d d d d d d d d d 
+d d d d f d d d d d d f d d d 
+. d d d f d d d d d f f d d . 
+. d d d f f f d d f f d d d . 
+. . d d d d f f f f d d d . . 
+. . . . d d d d d d d . . . . 
+. . . . . . . . . . . . . . . 
 `];
-            case "myAnim2":
-            case "anim4":return [img`
-. . . f f f f f f f f . . . . . 
-. . f f f f f f f f f f . . . . 
-. f f f f f f f f f f f f . . . 
-. f f f f f f f f f f f f . . . 
-f f f f f f f f f f f f f . . . 
-f f f f f f f f f f f f f . . . 
-d d d f f f f f f f d f d . . . 
-d d d d d d d d d d d d d . . . 
-d d d d d d d d d d d d d . . . 
-. d d d d d d d d d d d d . . . 
-. d d d d d d d d d d d . . . . 
-. d d d d d d d d d d d . . . . 
-. . d d d d d d d d d d . . . . 
-. . d d d d d d d d d . . . . . 
-. . d d d d d d d d d . . . . . 
-. . . d d d d d d . . . . . . . 
-. . . . . . . . . . . . . . . . 
+            case "myAnim7":
+            case "anim9":return [img`
+. . . . f f f f f f f . . . . 
+. . f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f . 
+. f f f f f f f f f f f f f . 
+f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f 
+d f f f f f f f f f f f f f f 
+d d f f f f f f f f f f f d d 
+d d d f f f f f f f f f d d d 
+d d d d d d f f d d d d d d d 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+. . d d d d d d d d d d d . . 
+. . . . d d d d d d d . . . . 
+. . . . . . . . . . . . . . . 
 `, img`
-. . . . . . . . . . . . . . . . 
-. . . f f f f f f f f . . . . . 
-. . f f f f f f f f f f . . . . 
-. f f f f f f f f f f f f . . . 
-. f f f f f f f f f f f f . . . 
-f f f f f f f f f f f f f . . . 
-f f f f f f f f f f f f f . . . 
-d d d f f f f f f f d f d . . . 
-d d d d d d d d d d d d d . . . 
-d d d d d d d d d d d d d . . . 
-. d d d d d d d d d d d d . . . 
-. d d d d d d d d d d d . . . . 
-. d d d d d d d d d d d . . . . 
-. . d d d d d d d d d d . . . . 
-. . d d d d d d d d d . . . . . 
-. . d d d d d d d d d . . . . . 
-. . . d d d d d d . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . 
+. . f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f . 
+. f f f f f f f f f f f f f . 
+f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f 
+d f f f f f f f f f f f f f f 
+d d f f f f f f f f f f f d d 
+d d d f f f f f f f f f d d d 
+d d d d d d f f d d d d d d d 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+. . d d d d d d d d d d d . . 
+. . . . d d d d d d d . . . . 
+`];
+            case "myAnim5":
+            case "anim7":return [img`
+. . . f f f f f f f f . . . . 
+. . f f f f f f f f f f f . . 
+. 1 f f f f f f f f f 1 f f . 
+. f f f f 1 f f f 1 f f f f f 
+f f 1 f f 1 f f f 1 f f 1 f f 
+1 f 1 2 2 2 2 f f f f f f f f 
+1 2 2 2 f f 2 2 1 2 2 2 2 f 1 
+1 2 2 1 f f 1 2 2 1 f f 1 2 1 
+1 1 2 2 2 2 2 2 2 1 f f 1 2 1 
+1 1 1 2 2 1 1 1 1 1 2 2 2 2 1 
+1 1 2 2 2 2 1 1 1 1 1 2 2 2 1 
+. 1 1 2 1 1 2 2 2 2 2 2 1 2 . 
+. 1 1 2 1 1 1 1 1 1 1 1 1 2 . 
+. . 1 1 2 1 1 1 1 1 1 2 2 . . 
+. . . . 2 2 2 2 2 2 2 2 . . . 
+`];
+            case "myAnim8":
+            case "anim10":return [img`
+. . . . d d d d d d d . . . . 
+. . d d d d d d d d d d d . . 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+d d d d d d d d d d d d d d d 
+d d d d 2 d d d d d 2 d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d f d d d d d f d d d d 
+. d d d f f d d d f f d d d . 
+. d d d d d f f f f d d d d . 
+. . d d d d d d d d d d d . . 
+. . . . d d d d d d d . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . d d d d d d d . . . . 
+. . d d d d d d d d d d d . . 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+d d d d d d d d d d d d d d d 
+d d d d 2 d d d d d 2 d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d f d d d d d f d d d d 
+. d d d f f d d d f f d d d . 
+. d d d d d f f f f d d d d . 
+. . d d d d d d d d d d d . . 
+. . . . d d d d d d d . . . . 
+`];
+            case "myAnim9":
+            case "anim11":return [img`
+. . . . d d d d d d d . . . . 
+. . d d d d d d d d d d d . . 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+. . d d d d d d d d d d d . . 
+. . . . d d d d d d d . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . d d d d d d d . . . . 
+. . d d d d d d d d d d d . . 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d 
+. d d d d d d d d d d d d d . 
+. d d d d d d d d d d d d d . 
+. . d d d d d d d d d d d . . 
+. . . . d d d d d d d . . . . 
+`];
+            case "myAnim11":
+            case "anim13":return [img`
+. . f f f f f f f f f f f f f . 
+. . f f f f f f f f f f f f f f 
+. f f f f e e e e e f f f f f f 
+f f f f e f e e e e f e e f f f 
+f f f e f e f e e f e f e f f f 
+f f f e f e f e e f e f e f f f 
+f f f e f f f e e f f f e e f f 
+f f f e f e f e e f e f e e f f 
+f f f e e f e e e e f e e e f f 
+f f f e e e e e f e e e e e f f 
+f f f e e e e f e f e e e e f f 
+. . . e e e e f 1 f e e e f f . 
+. . . e e e e f 1 f e e e . . . 
+. . . e e e e f 1 f e e . . . . 
+. . . . e e e e f e e e . . . . 
+. . . . . e e e e e e . . . . . 
 `];
         }
         return null;
     })
 
 }
-// 自动生成的代码。请勿编辑。
+// Auto-generated code. Do not edit.
